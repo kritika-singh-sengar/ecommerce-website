685ce5cff94caa69460a8dae8c0af1760535e592
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-08-16 18:42:41 +0530

    frontend application

---
+++ FrontendApplication/src/app/constants/constants.ts
@@ -0,0 +1,17 @@
+export const Constants={
+    APPROVED : "approved",
+    PENDING : "pending",
+    UNAVAILABLE : "unavailable",
+
+    //for running in docker
+    // userApiURL : 'http://ecommerce-backend:8082/api/v1/user/',
+    // poductApiURL: 'http://ecommerce-backend:8082/api/v1/user/product/',
+    // reviewAPiURL: 'http://ecommerce-backend:8082/api/v1/user/review/',
+    // adminApiURL : 'http://ecommerce-backend:8082/api/v1/admin/',
+
+    //for running locally
+    userApiURL : 'http://localhost:8082/api/v1/user/',
+    poductApiURL: 'http://localhost:8082/api/v1/user/product/',
+    reviewAPiURL: 'http://localhost:8082/api/v1/user/review/',
+    adminApiURL : 'http://localhost:8082/api/v1/admin/',
+}
